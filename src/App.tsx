9d30bb201c96857c6b298bf8319446863ebd2359
skipper-switch <oluwatosin.adegoroye.interswitchng.com>
2026-03-19 14:09:51 +0100

    feat: all set

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,62 @@
-import { Button } from "@/components/ui/button"
+import { lazy, Suspense } from "react"
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom"
+import Providers from "./providers/providers"
+import PageLoader from "./components/common/page-loader"
+
+// Lazy load pages for code splitting
+const Home = lazy(() => import("./pages/home"))
+const Login = lazy(() => import("./pages/login"))
+const NotFound = lazy(() => import("./pages/not-found"))
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+
+      {/* Private Users Route */}
+      {/* <Route path="" element={<PrivateUserRoute />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="transactions" element={<Transactions />} />
+
+          <Route path="settings" element={<Settings />}>
+            <Route index element={<AccountSettings />} />
+            <Route path="security-settings" element={<SecuritySettings />} />
+          </Route>
+        </Route>  */}
+
+      {/* Private Admin Route */}
+      {/* <Route path="/backoffice" element={<AdminLayout />}>
+          <Route index element={<AdminLogin />} />
+          
+          <Route path="dashboard" element={<ProtectedAdminLayout />}>
+            <Route index element={<AdminDashboard />} />
+            <Route path="users" element={<Users />} />
+            <Route path="transactions" element={<AdminTransactions />} />
+
+            <Route path="settings" element={<Settings />}>
+              <Route index element={<AccountSettings />} />
+              <Route path="security-settings" element={<SecuritySettings />} />
+            </Route>
+          </Route>
+        </Route> */}
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+)
 
 export function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
+    <Providers>
+      <Suspense fallback={<PageLoader />}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </Providers>
   )
 }
 
